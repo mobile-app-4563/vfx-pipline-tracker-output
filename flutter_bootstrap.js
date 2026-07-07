5b88e5317b3f5ac9cf949694cee1e66868d336a9
mobile-app-4563 <mobileapp.dev.mohamedfasil@gmail.com>
2026-07-07 19:18:50 +0530

    fixxes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "130102507" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3449430833" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
